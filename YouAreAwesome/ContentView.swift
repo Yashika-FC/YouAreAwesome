647dcf44c80908b1f4cc8f6dccae188854657007
yashikaaggarwalin <yashika.aggarwal.in@freechargebiz.in>
2026-05-10 23:06:08 +0530

    Learnt about var and let, scope, created one button toggle using if-else conditionals

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,47 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    Inside Struct Curlies , so have structwide scopes and can be used widely anywhere within this struct.. also with any views inside this struct  like Image , Text and Button here
+    
     @State private var message = "You Are Awesome!";
+    @State private var toggle = true;
+    @State private var img:String="swift"
+    
+//    anything defined here is structwide and can be used anywhere within this struct 
     
     var body: some View {
         VStack{
             Spacer()
-                
-                
-            Image(systemName: "swift")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.orange)
-                    .frame(width: 200, height: 200)
-                    .padding()
+            
+            
+            Image(systemName: img)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+                .padding()
             
             
             Text(message)
-                    .font(.largeTitle)
-                    .fontWeight(.ultraLight)
-                    .foregroundStyle(.black)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
             
             Spacer()
             
-            HStack{
-                Button("Awesome!") {
-                    message="Awesome!"
-                    print("You clicked me!")
-                    print(message)
+            
+            
+            
+            Button("Press Me!") {
+                if (toggle==true){
+                    message="You Are Awesome!"
+                    img="sun.max.fill"
                 }
+                else{
+                    message="You Are Great!"
+                    img="hand.thumbsup.fill"
+                }
+                toggle = !toggle
+                print("You clicked -> \(message)")
                 
-                Button("Great!") {
-                    message="Great!"
-                    print("You clicked me!")
-                    print(message)
-                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .fontWeight(.heavy)
             .foregroundStyle(.white)
             .tint(.orange)
-        
+            
         }
         .padding()
     }
