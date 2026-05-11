dae8204746506043e94c467e1b77194e7a383291
yashikaaggarwalin <yashika.aggarwal.in@freechargebiz.in>
2026-05-11 12:45:03 +0530

    Used Ternary Operator instead of if-else, just two lines of code

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    Inside Struct Curlies , so have structwide scopes and can be used widely anywhere within this struct.. also with any views inside this struct  like Image , Text and Button here
+    //    Inside Struct Curlies , so have structwide scopes and can be used widely anywhere within this struct.. also with any views inside this struct  like Image , Text and Button here
     
     @State private var message = "You Are Awesome!";
     @State private var toggle = true;
     @State private var img:String="swift"
     
-//    anything defined here is structwide and can be used anywhere within this struct 
+    //    anything defined here is structwide and can be used anywhere within this struct
     
     var body: some View {
         VStack{
@@ -41,14 +41,20 @@ struct ContentView: View {
             
             
             Button("Press Me!") {
-                if (toggle==true){
-                    message="You Are Awesome!"
-                    img="sun.max.fill"
-                }
-                else{
-                    message="You Are Great!"
-                    img="hand.thumbsup.fill"
-                }
+                
+                //                Using Ternary Operator
+                message=toggle ? "You Are Awesome!" : "You Are Great!"
+                img=toggle ? "sun.max.fill" : "hand.thumbsup.fill"
+                
+                //                if (toggle==true){
+                //                    message="You Are Awesome!"
+                //                    img="sun.max.fill"
+                //                }
+                //                else{
+                //                    message="You Are Great!"
+                //                    img="hand.thumbsup.fill"
+                //                }
+                
                 toggle = !toggle
                 print("You clicked -> \(message)")
                 
