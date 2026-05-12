c978e38d600dc255509f605b47dd9f6234d79646
yashikaaggarwalin <yashika.aggarwal.in@freechargebiz.in>
2026-05-12 12:00:58 +0530

    Implemented Arrays concept for dynamic lenght array of messages

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -17,10 +17,12 @@ struct ContentView: View {
     
     //    In swift var can hold only one type of data, if we create variable to hold a string, later on we cannot put a number to it
     @State private var imgNum = 0;
+    @State private var msgNum = 0;
     
     //    Since Now we are not using SystemName keyword (which works for Swift inbuilt images and icons), so now we cant set starting image as "swift" or any inbuilt image
     
     @State private var img = "";
+    @State private var msgsArr : [String] = ["You Are Awesome!", "Fabulous? That\'s You!", "You Are Marvelous!", "You Make Me Smile!", "I Think You\'re Magic!", "You Are Skilled", "You Are so Kind!", "You Are Perfect!" ,"You are the Best Version of Yourself!","You Are The Best!)"]
     
     
     //    anything defined here is structwide and can be used anywhere within this struct
@@ -36,11 +38,13 @@ struct ContentView: View {
                 .clipShape(.rect(cornerRadius: 20))
                 .shadow(radius: 100)
             
-            
+//            Text Views are left justified By Default, Vstack makes one liners look centered alinged , but multilined text looks left-justified itelself
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+//            To make even multilined text center (as it is left aligned by default)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
@@ -50,8 +54,8 @@ struct ContentView: View {
             Button("Show Message!") {
                 
                 //                Using Ternary Operator
-                message=toggle ? "You Are Awesome!" : "You Are Great!"
-                img="image\(imgNum)"
+                //                message=toggle ? "You Are Awesome!" : "You Are Great!"
+                //                img="image\(imgNum)"
                 
                 //                if (toggle==true){
                 //                    message="You Are Awesome!"
@@ -62,16 +66,26 @@ struct ContentView: View {
                 //                    img="hand.thumbsup.fill"
                 //                }
                 
-                toggle = !toggle
+                //                toggle = !toggle
+                
+                message=msgsArr[msgNum]
+                img="image\(imgNum)"
                 
                 //TODO: -Update the image name variable-
-                print("This is the \(imgNum)th click!")
+                print("This is the \(imgNum)th image and \(msgNum)th msg!")
+                print("You clicked -> \(message)")
+                
                 imgNum+=1;
                 if (imgNum>9){
                     imgNum=0;
                 }
-
-                print("You clicked -> \(message)")
+                
+                msgNum+=1;
+                if msgNum>msgsArr.count-1{
+                    msgNum=0;
+                }
+                
+                
                 
             }
             .buttonStyle(.borderedProminent)
