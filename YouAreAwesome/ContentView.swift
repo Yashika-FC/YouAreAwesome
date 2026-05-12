d9c5f7b59e8126168f194f0f532aa50debcc2a77
yashikaaggarwalin <yashika.aggarwal.in@freechargebiz.in>
2026-05-12 16:37:22 +0530

    Learnt about minScaleFactor to wrap  text in given frame dimensions , Image and Text animations

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -29,22 +29,32 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            Spacer()
             
+            
+            //            Text Views are left justified By Default, Vstack makes one liners look centered alinged , but multilined text looks left-justified itelself
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            //            To make even multilined text center (as it is left aligned by default)
+                .multilineTextAlignment(.center)
+            
+            //            minimum allowed height to be reduced to is 50% of the original, if the complete text not appears with original height in the frame height, setting it below 0.5 looks unprofessional
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+//            default duration of any animation whether image or text or anything is 0.35sec
+//            .easeInOut is same as default animation with duration property, .linear is the normal animation
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(img)
                 .resizable()
                 .scaledToFit()
                 .clipShape(.rect(cornerRadius: 20))
                 .shadow(radius: 100)
+//            keep extraBounce to 0.2 or below to avoid distortion
+                .animation(.bouncy(duration: 0.4, extraBounce: 0.2), value: img)
             
-//            Text Views are left justified By Default, Vstack makes one liners look centered alinged , but multilined text looks left-justified itelself
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-//            To make even multilined text center (as it is left aligned by default)
-                .multilineTextAlignment(.center)
+            
             
             Spacer()
             
